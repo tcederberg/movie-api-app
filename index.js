fc7074102e283c0bc1a664fb30423654d72bfd3b
tcederberg <trcederberg@gmail.com>
2023-11-27 07:02:31 -0500

    Update to movies endpoint

--- index.js
+++ index.js
@@ -39,9 +39,8 @@ app.get('/', (req, res) => {
     res.send('Welcome to myFlix!');
 });
 
-app.get('/movies', passport.authenticate('jwt', { session: false }),
-    async (req, res) => {
-    await Movies.find({})
+app.get('/movies', (req, res) => {
+    Movies.find({})
         .then((movies) => {
             res.status(200).json(movies);
         })
